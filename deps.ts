4be4389d44f7c99168ab96a2439a2b684c58f105
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-14 22:41:07 +0900

    update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
-export * as esbuild from 'https://deno.land/x/esbuild@v0.17.15/mod.js';
+export * as esbuild from 'https://deno.land/x/esbuild@v0.17.16/mod.js';
 export * as cache from 'https://deno.land/x/cache@0.2.13/mod.ts';
+export { posix } from 'https://deno.land/std@0.183.0/path/mod.ts';
