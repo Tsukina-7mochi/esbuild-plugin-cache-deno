0cb4df264ad378360a938e3ec1530860b74fcbd0
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-07-10 10:01:43 +0900

    [release patch] Updated dependencies

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as esbuild from 'https://deno.land/x/esbuild@v0.18.1/mod.js';
-export { posix } from 'https://deno.land/std@0.191.0/path/mod.ts';
-export * as fs from 'https://deno.land/std@0.191.0/fs/mod.ts';
-export { crypto } from 'https://deno.land/std@0.191.0/crypto/mod.ts';
-export * as asserts from 'https://deno.land/std@0.191.0/testing/asserts.ts';
+export * as esbuild from 'https://deno.land/x/esbuild@v0.18.11/mod.js';
+export { posix } from 'https://deno.land/std@0.193.0/path/mod.ts';
+export * as fs from 'https://deno.land/std@0.193.0/fs/mod.ts';
+export { crypto } from 'https://deno.land/std@0.193.0/crypto/mod.ts';
+export * as asserts from 'https://deno.land/std@0.193.0/testing/asserts.ts';
