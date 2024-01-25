7b9d1c172bfa4fea91eb719420cbb2fa2b5f697f
Tsukina-7mochi <tsukina7mochi@gmail.com>
2024-01-25 20:19:54 +0900

    chore: add exmplae of ignoring modules

---
+++ example/ignore-modules/build.ts
@@ -0,0 +1,22 @@
+import { esbuild } from '../../deps.ts';
+import esbuildCachePlugin from '../../mod.ts';
+
+const lockMap = JSON.parse(Deno.readTextFileSync('./deno.lock'));
+
+const denoPath = await esbuildCachePlugin.util.getDenoDir();
+await esbuild.build({
+  entryPoints: ['./index.ts'],
+  bundle: true,
+  outfile: './bundle.js',
+  plugins: [
+    esbuildCachePlugin({
+      denoCacheDirectory: denoPath,
+      lockMap,
+      loaderRules: [
+        { test: /^node:/, loader: 'empty' },
+      ],
+    }),
+  ],
+});
+
+esbuild.stop();
---
+++ example/ignore-modules/index.ts
@@ -0,0 +1,4 @@
+import 'node:fs';
+import 'node:util';
+
+console.log('/index.ts');
