d5ce2d119e2fb23f86334851ba3c72dcac2059f2
Tsukina-7mochi <tsukina7mochi@gmail.com>
2024-01-25 20:52:34 +0900

    chore: add example of lit via esm.sh

---
+++ example/lit-esmsh/my-counter.ts
@@ -0,0 +1,27 @@
+import { LitElement, html } from 'lit';
+// decorators are not currently supported by esbuild
+// import { customElement, property } from 'lit/decorators.js';
+
+export class MyCounter extends LitElement {
+  static properties = {
+    count: { type: Number }
+  }
+
+  declare count: number;
+
+  constructor() {
+    super();
+    this.count = 0;
+  }
+
+  private _increment() {
+    this.count += 1;
+  }
+
+  render() {
+    return html`
+      <button @click=${this._increment}>Count: ${this.count}</button>
+    `;
+  }
+}
+customElements.define('my-counter', MyCounter);
