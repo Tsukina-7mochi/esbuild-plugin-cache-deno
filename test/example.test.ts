a23b389aa02dfdeb60bfa8b6f2decb8b8d583526
Tsukina-7mochi <tsukina7mochi@gmail.com>
2024-02-02 17:56:12 +0900

    test: refactor test of examples

--- test/example.test.ts
+++ test/example.test.ts
@@ -7,11 +7,7 @@ import { assertEquals } from 'assert';
 import { path } from '../deps.ts';
 import examples from '../example/examples.json' assert { type: 'json' };
 
-const exampleNames = Object.entries(examples)
-  .filter(([_, value]) => value.type === 'simple')
-  .map(([key]) => key);
-
-const executeDenoTask = async function(cwd: string, taskName: string, ...args: string[]) {
+const executeDenoTask = function(cwd: string, taskName: string, ...args: string[]): Promise<Deno.CommandOutput> {
   const process = new Deno.Command(Deno.execPath(), {
     args: ['task', taskName, ...args],
     cwd,
@@ -19,42 +15,55 @@ const executeDenoTask = async function(cwd: string, taskName: string, ...args: s
     stdout: "piped",
     stderr: "piped",
   }).spawn();
-  return await process.output();
+
+  return process.output().then((output) => new Promise<typeof output>((resolve, reject) => {
+    if(output.success) {
+      resolve(output);
+    } else {
+      reject(output);
+    }
+  }));
 }
 
 Deno.test("examples", async (testContext) => {
   const decoder = new TextDecoder();
 
-  for(const exampleName of exampleNames) {
+  for(const [exampleName, exampleSettings] of Object.entries(examples)) {
     const examplePath = path.resolve('example', exampleName);
     await testContext.step(exampleName, async () => {
-      const cleanOutput = await executeDenoTask(examplePath, 'clean');
-      if(!cleanOutput.success) {
-        const stdout = decoder.decode(cleanOutput.stdout);
-        const stderr = decoder.decode(cleanOutput.stderr);
-        throw Error(`Clean process failed with:\n Error ${stdout}\n Output ${stderr}`);
+      await executeDenoTask(examplePath, 'clean')
+        .catch((output) => {
+          const stdout = decoder.decode(output.stdout);
+          const stderr = decoder.decode(output.stderr);
+          throw Error(`Clean process failed with:\n Error ${stdout}\n Output ${stderr}`);
+        });
+
+      await executeDenoTask(examplePath, 'build')
+        .catch((output) => {
+          const stdout = decoder.decode(output.stdout);
+          const stderr = decoder.decode(output.stderr);
+          throw Error(`Build process failed with:\n Error ${stdout}\n Output ${stderr}`);
+        });
+
+      if(exampleSettings.type !== 'simple') {
+        return;
       }
 
-      const buildOutput = await executeDenoTask(examplePath, 'build');
-      if(!buildOutput.success) {
-        const stdout = decoder.decode(buildOutput.stdout);
-        const stderr = decoder.decode(buildOutput.stderr);
-        throw Error(`Build process failed with:\n Error ${stdout}\n Output ${stderr}`);
-      }
-
-      const runRawOutput = await executeDenoTask(examplePath, 'run:raw');
+      const runRawOutput = await executeDenoTask(examplePath, 'run:raw')
+        .catch((output) => {
+          const stdout = decoder.decode(output.stdout);
+          const stderr = decoder.decode(output.stderr);
+          throw Error(`Execution (raw) process failed with:\n Error ${stdout}\n Output ${stderr}`);
+        });
       const runRawStdout = decoder.decode(runRawOutput.stdout);
-      if(!runRawOutput.success) {
-        const stderr = decoder.decode(runRawOutput.stderr);
-        throw Error(`Build process failed with:\n Error ${runRawStdout}\n Output ${stderr}`);
-      }
 
-      const runBundleOutput = await executeDenoTask(examplePath, 'run:bundle');
+      const runBundleOutput = await executeDenoTask(examplePath, 'run:bundle')
+        .catch((output) => {
+          const stdout = decoder.decode(output.stdout);
+          const stderr = decoder.decode(output.stderr);
+          throw Error(`Execution (bundle) process failed with:\n Error ${stdout}\n Output ${stderr}`);
+        });
       const runBundleStdout = decoder.decode(runBundleOutput.stdout);
-      if(!runBundleOutput.success) {
-        const stderr = decoder.decode(runBundleOutput.stderr);
-        throw Error(`Build process failed with:\n Error ${runBundleStdout}\n Output ${stderr}`);
-      }
 
       assertEquals(runRawStdout, runBundleStdout);
     });
