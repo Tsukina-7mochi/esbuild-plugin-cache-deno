e3bca446119dbf02a50e6cf132f5293d5453a573
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-06-10 15:47:02 +0900

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as esbuild from 'https://deno.land/x/esbuild@v0.20.1/mod.js';
-export * as path from 'https://deno.land/std@0.219.0/path/mod.ts';
-export * as fs from 'https://deno.land/std@0.219.0/fs/mod.ts';
-export { crypto } from 'https://deno.land/std@0.219.0/crypto/mod.ts';
+export * as esbuild from 'https://deno.land/x/esbuild@v0.21.5/mod.js';
+export * as path from 'https://deno.land/std@0.224.0/path/mod.ts';
+export * as fs from 'https://deno.land/std@0.224.0/fs/mod.ts';
+export { crypto } from 'https://deno.land/std@0.224.0/crypto/mod.ts';
